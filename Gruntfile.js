5e503b458cc2e45c400d91588ec0759d44917b77
Aimee <aimeebethmj@gmail.com>
2015-11-20 12:46:13 +0000

    Indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,50 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        shell: {
-            jekyllBuild: {
-                command: 'jekyll build'
-            }
-        },
-        connect: {
-            server: {
-                options: {
-                    port: 8080,
-                    base: '_site'
-                }
-            }
-        },
-        sass: {
-          dist: {
-            options: {
-              style: 'expanded'
-            },
-            files: {
-              'sassy.css': '_sass/sassy.scss'
-            }
-          }
-        },
-        watch: {
-          livereload: {
-            files: [
-              '_config.yml',
-              'index.html',
-              '*.css',
-              '_sass/**',
-              '_layouts/**',
-              '_posts/**',
-              '_includes/**'
-            ],
-            tasks: ['sass', 'shell:jekyllBuild'],
-            options: {
-              livereload: true
-            },
-          },
+  grunt.initConfig({
+    shell: {
+        jekyllBuild: {
+            command: 'jekyll build'
         }
-    })
+    },
+    connect: {
+        server: {
+            options: {
+                port: 8080,
+                base: '_site'
+            }
+        }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'sassy.css': '_sass/sassy.scss'
+        }
+      }
+    },
+    watch: {
+      livereload: {
+        files: [
+          '_config.yml',
+          'index.html',
+          '*.css',
+          '_sass/**',
+          '_layouts/**',
+          '_posts/**',
+          '_includes/**'
+        ],
+        tasks: ['sass', 'shell:jekyllBuild'],
+        options: {
+          livereload: true
+        },
+      },
+    }
+  })
 
-    grunt.loadNpmTasks('grunt-contrib-sass')
-    grunt.loadNpmTasks('grunt-contrib-connect')
-    grunt.loadNpmTasks('grunt-contrib-watch')
-    grunt.loadNpmTasks('grunt-shell')
-    grunt.registerTask('default', ['shell', 'connect', 'watch'])
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-connect')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-shell')
+  grunt.registerTask('default', ['shell', 'connect', 'watch'])
 }
